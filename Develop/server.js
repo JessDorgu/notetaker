6c230eca16ca494b832133f6ad1317374aa030bb
unknown <jdorgu011@aol.com>
2023-08-07 00:04:58 -0400

    add routes to index and notes html

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,25 @@
-console.log("yep")
+console.log("yep");
+
+const express = require ('express');
+const path = require('path');
+const app = express();
+
+const PORT = 3005;
+
+//static files
+app.use(express.static('public'));
+
+// create routes to connect the index and notes html
+
+app.get('/', (req, res) => res.send('Navigate to /notes'));
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.json());
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
