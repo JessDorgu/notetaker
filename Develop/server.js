e803b7e5bcf9567e72c1161a91e8723ebe1853db
unknown <jdorgu011@aol.com>
2023-09-06 20:59:02 -0400

    added api routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,9 @@
 const express = require ('express');
-
+const fs = require("fs");
+const uuid = require("uuid");
 
 const app = express();
-
-const PORT = 3008
-;
+const PORT = 3008;
 
 //static files
 app.use(express.static('public'));
@@ -21,7 +20,43 @@ app.get('/notes', (req, res) => {
   res.sendFile(__dirname + '/public/notes.html');
 });
 
+app.get("/api/notes", (req,res) =>{
+  fs.readFile("db/db.json", "utf8",(err,data)=>{
+      if(err){
+          console.error(err);
+          res.statusCode(500).json({error:"Something is wrong with the internal server"});
+      } else{
+          const notes = JSON.parse(data);
+          res.json(notes);
+      }
+  });
 
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid.v4(); 
+
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Something is wrong with the internal server' });
+    } else {
+      const notes = JSON.parse(data);
+     
+      notes.push(newNote);
+
+      fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Something is wrong with the internal server' });
+        } else {
+          res.json(newNote);
+        }
+      });
+    }
+  });
+});
 
 
 app.listen(PORT, () =>
